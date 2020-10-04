19b1b5a43cbc6628e850f4ad60d53296f3bfe27a
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-10-04 22:44:21 +0300

    f

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const config = require('./config');
+
+
+
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+})
+
+
+async function Start(){
+    app.listen(config.PORT,(err)=>console.log(err||`Server is start ${config.PORT}`))
+}
+
+Start()
